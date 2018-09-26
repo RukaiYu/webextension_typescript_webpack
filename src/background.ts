29868bd7f1cefd5f46bbf9b48234975da3d422be
lhk <lars.klein@rwth-aachen.de>
2018-09-26 20:25:08 +0200

    adding comments

--- src/background.ts
+++ src/background.ts
@@ -5,10 +5,19 @@ let numberContentScripts = 0;
 
 
 function main(){
+    // here we communicate with the content script
     browser.runtime.onMessage.addListener((message, sender, sendResponse)=>{
       numberContentScripts++;
       sendResponse(`wow, you have executed the content_script for ${numberContentScripts} times`);
     });
 }
 
+// we use the module util
+// if this is not done, webpack will not include the code for util
+// at least not for the background script
+// that means if you debug the background, you don't see sources for util
+// if you debug the content_script, the util sources might still be there
+console.log(happy());
+
+// set it all up
 main();
