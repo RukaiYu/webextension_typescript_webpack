55b65035fec513bb8d8d5a0964b2877211b07dc9
lhk <lars.klein@rwth-aachen.de>
2018-09-27 10:32:19 +0200

    wip: fiddling with webpack config

--- src/util.ts
+++
@@ -1,4 +0,0 @@
-// webpack can deal with modules
-export const happy = (): string => {
-  return "hey this is modular";
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,94 @@
 const webpack = require("webpack");
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, "src")
-const DIST_DIR = path.join(__dirname, "dist/js")
+const SRC_DIR = path.join(__dirname, "src");
+const DIST_DIR = path.join(__dirname, "dist");
+
+// don't forget to add asset extensions here, if you want them copied over to dist
+const ASSET_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
+
+const IS_PRODUCTION = false;
+
 
 module.exports = {
+    // there are three entry points, for the three components of our plugin
     entry: {
-        content_script: path.join(SRC_DIR, 'content_script.ts'),
-        background: path.join(SRC_DIR, 'background.ts')
+        content_script: path.join(SRC_DIR, 'content/index.ts'),
+        background: path.join(SRC_DIR, 'background/index.ts'),
+        popup: path.join(SRC_DIR, "popup/index.ts")
     },
+    // each entry point will be compiled to a correspondingly named output
     output: {
         path: DIST_DIR,
         filename: '[name].js'
     },
+    // in addition to that, these rules will carry html, css, etc over to dist
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            use: 'ts-loader'
-        },
-        {
-            test: require.resolve('webextension-polyfill'),
-            use: 'imports-loader?browser=>undefined'
-          }]
+        rules: [
+
+            // a rule for typescript code
+            {
+                test: /\.tsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: 'ts-loader'
+            },
+
+            // a rule for html
+            {
+                test: /\.html$/,
+                use: [
+                    'file-loader',
+                    'extract-loader',
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: IS_PRODUCTION,
+                            attrs: [
+                                'link:href',
+                                'script:src',
+                                'img:src'
+                            ]
+                        }
+                    }
+                ]
+            },
+
+            // a rule for css
+            {
+                test: /\.css$/,
+                use: [
+                    'file-loader',
+                    'extract-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            minimize: IS_PRODUCTION
+                        }
+                    }
+                ]
+            },
+
+            // a rule for assets
+            {
+                test: new RegExp('\.(' + ASSET_EXTENSIONS.join('|') + ')$'),
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets/'
+                    }
+                }
+            },
+
+            // a trick to include the webext polyfill if necessary
+            {
+                test: require.resolve('webextension-polyfill'),
+                use: 'imports-loader?browser=>undefined'
+            }]
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
-    plugins:[
+    plugins: [
         new webpack.ProvidePlugin({
             browser: "webextension-polyfill"
         })
